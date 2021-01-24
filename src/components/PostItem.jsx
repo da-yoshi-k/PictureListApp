3e6c75d39b3a6462f625fad2bd39a4f8b713c70f
da-yoshi-k <hey234234s@gmail.com>
2021-01-25 00:07:40 +0900

    投稿のcomponet化とFlatList導入

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
+
+// const imgPath = require('../../assets/sample.jpg');
+// const imgPath2 = require('../../assets/sample2.jpg');
+
+export default function PostItem() {
+  const [dataSource, setDataSource] = useState([]);
+
+  useState(() => {
+    // TODO 削除
+    // ダミーの投稿を表示させるための記述
+    // eslint-disable-next-line
+    const items = Array.apply(null, Array(60)).map((v, i) => ({
+      id: i,
+      src: 'https://placehold.jp/120x120.png',
+    }));
+    setDataSource(items);
+  }, []);
+
+  return (
+    <FlatList
+      data={dataSource}
+      renderItem={({ item }) => (
+        <View style={styles.postItem}>
+          <Image
+            style={styles.pictureImg}
+            resizeMode="cover"
+            source={{ uri: item.src }}
+          />
+          <Text>☺ yamada taro</Text>
+          <Text>2021年1月24日 22:00</Text>
+          <Text>投稿のタイトル</Text>
+        </View>
+      )}
+      numColumns={2}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  postItem: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '50%',
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    height: 200,
+  },
+  pictureImg: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 160,
+    height: 120,
+  },
+});
--- src/screens/PictureListScreen.jsx
+++ src/screens/PictureListScreen.jsx
@@ -1,35 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
-
-const imgPath = require('../../assets/sample.jpg');
-const imgPath2 = require('../../assets/sample2.jpg');
+import { StyleSheet, View } from 'react-native';
+import PostItem from '../components/PostItem';
 
 export default function PictureListScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.postItem}>
-        <Image style={styles.pictureImg} resizeMode="cover" source={imgPath} />
-        <Text>☺ yamada taro</Text>
-        <Text>2021年1月24日 22:00</Text>
-        <Text>美味しいコーヒー</Text>
-      </View>
-      <View style={styles.postItem}>
-        <Image style={styles.pictureImg} resizeMode="cover" source={imgPath2} />
-      </View>
-      <View style={styles.postItem}>
-        <Image
-          style={styles.pictureImg}
-          resizeMode="cover"
-          source={{ uri: 'https://placehold.jp/120x120.png' }}
-        />
-      </View>
-      <View style={styles.postItem}>
-        <Image
-          style={styles.pictureImg}
-          resizeMode="cover"
-          source={{ uri: 'https://placehold.jp/120x120.png' }}
-        />
-      </View>
+      <PostItem />
     </View>
   );
 }
@@ -37,19 +13,9 @@ export default function PictureListScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    paddingHorizontal: 12,
-    paddingVertical: 12,
-  },
-  postItem: {
-    width: '50%',
-    paddingHorizontal: 8,
-    height: 200,
-  },
-  pictureImg: {
-    alignSelf: 'center',
-    width: 160,
-    height: 120,
+    // flexDirection: 'row',
+    // flexWrap: 'wrap',
+    // paddingHorizontal: 12,
+    // paddingVertical: 12,
   },
 });
